cfd32ece3350d1be3f0aaa40fd4f86494d318bd6
Max Fulton <mgfulton@umn.edu>
2015-10-12 11:24:26 -0500

    Visual Haiku

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	
+	$('.main').okzoom({
+  		width: 400,
+  		height: 400,
+ 	 	round: true,
+  		background: "#fff",
+  		backgroundRepeat: "no-repeat",
+  		shadow: "0 0 5px #000",
+  		border: "1px solid black"
+	});
+
+
+});
